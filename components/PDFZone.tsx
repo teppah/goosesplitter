864f47ebdd6c437113f167f36c37914e22ae185b
Yi Feng Yang <yifeng@yfyang.dev>
2020-10-17 11:04:15 -0400

    Add pdf-aware text display

--- components/PDFZone.tsx
+++ components/PDFZone.tsx
@@ -1,5 +1,7 @@
-import { Dispatch, SetStateAction, useCallback } from "react";
+import { Dispatch, SetStateAction, useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import clsx from "clsx";
+
 import containerStyles from "styles/Container.module.css";
 import btnStyles from "styles/Button.module.css";
 
@@ -10,35 +12,47 @@ const PDFZone = ({
   data: Uint8Array;
   setData: Dispatch<SetStateAction<Uint8Array>>;
 }) => {
+  const [filename, setFilename] = useState("");
   const onDrop = useCallback(async (files: File[]) => {
     const first = files[0];
     let stream = await first.arrayBuffer();
     let newArray = new Uint8Array(stream);
     setData(newArray);
+    setFilename(first.name);
   }, []);
   const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
     onDrop,
     accept: ["application/pdf"],
   });
+
+  let zoneName = clsx({
+    [`${containerStyles.container}`]: true,
+    zone: true,
+    over: isDragActive,
+  });
   return (
-    <div {...getRootProps()} className={containerStyles.container}>
+    <div {...getRootProps()} className={zoneName}>
       <input {...getInputProps()}></input>
       {isDragActive ? (
-        <p>Drop the file here</p>
+        <p>Drop the PDF here</p>
       ) : (
         <div>
-          <p>Drag n drop here!</p>
-          <button type="button" onClick={open} className={btnStyles.btn}>
-            Open
-          </button>
+          <p>Drag your PDF here</p>
         </div>
       )}
+      <button type="button" onClick={open} className={btnStyles.btn}>
+        {filename || "Open"}
+      </button>
       <style jsx>{`
         h1 {
           @apply font-serif;
         }
-        .dropzone {
-          @apply p-4;
+        .zone > div {
+          @apply mb-1;
+        }
+        .over {
+          @apply bg-gray-400;
+          @apply border-solid border-gray-500;
         }
       `}</style>
     </div>
